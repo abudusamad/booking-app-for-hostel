30ec2621cfd6cfed7b2b52badc8743249d6d58e2
abudusamad <abudusamed@gmail.com>
2023-12-07 11:44:39 +0000

    making the axios post to the api

--- app/components/modal/register-modal.tsx
+++ app/components/modal/register-modal.tsx
@@ -1,14 +1,47 @@
 "use client";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import axios from "axios";
 import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const RegisterModal = () => {
-    const registerModal = useRegisterModal();
-    const [isLoading, setIsLoading] = useState(false);
-    return (<div>
-        
-    </div> );
-}
- 
+	const registerModal = useRegisterModal();
+	const [isLoading, setIsLoading] = useState(false);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FieldValues>({
+		defaultValues: {
+			email: "",
+			password: "",
+			firstName: "",
+			lastName: "",
+			phone: "",
+		},
+	});
+	const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+		setIsLoading(true);
+		axios
+			.post("/api/register", data)
+			.then(() => {
+				console.log("success");
+				registerModal.onClose();
+
+				setIsLoading(true);
+			})
+			.catch(() => {
+				console.log("error");
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	};
+
+	const bodyContent = <div className="flex flex-col gap-4"></div>;
+	return <div></div>;
+};
+
 export default RegisterModal;
