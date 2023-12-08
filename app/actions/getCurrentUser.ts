7b5f8b9ff1e14b0b2f89a939ef15efd8af30422c
abudusamad <abudusamed@gmail.com>
2023-12-08 12:58:09 +0000

    making the current user dynamic for diff users

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,31 +1,35 @@
+import prisma from "@/lib/prismadb";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import prisma from "@/lib/prismadb";
 
 export async function getSession() {
-    return await getServerSession(authOptions);
+	return await getServerSession(authOptions);
 }
 
 export default async function getCurrentUser() {
-    try {
-        const session = await getSession();
+	try {
+		const session = await getSession();
 
-        if (!session?.user?.email) {
-            return null;
-        }
+		if (!session?.user?.email) {
+			return null;
+		}
 
-        const currentUser = await prisma.user.findUnique({
-            where: {
-                email: session.user.email as string
-            },
-        });
-        if(!currentUser) {
-            return null;
-        }
+		const currentUser = await prisma.user.findUnique({
+			where: {
+				email: session.user.email as string,
+			},
+		});
+		if (!currentUser) {
+			return null;
+		}
 
-        return currentUser;
-        
-    } catch (error: any) {
-        return null;
-    }
+		return {
+			...currentUser,
+			createdAt: currentUser.createdAt.toISOString(),
+			updatedAt: currentUser.updatedAt.toISOString(),
+			emailVerified: currentUser.emailVerified?.toISOString(),
+		};
+	} catch (error: any) {
+		return null;
+	}
 }
