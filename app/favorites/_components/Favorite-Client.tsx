d6f9fff9bdd403fdded786cdd67499b18491da6d
abudusamad <abudusamed@gmail.com>
2023-12-14 20:32:44 +0000

    Making the favorites of the page

--- app/favorites/_components/Favorite-Client.tsx
+++ app/favorites/_components/Favorite-Client.tsx
@@ -1,26 +1,28 @@
 import { Container } from "@/app/components/Containter";
 import Heading from "@/app/components/Heading";
+import ListingCard from "@/app/components/listings/ListingCard";
 import { SafeListing, SafeUser } from "@/types";
 
 interface FavoritesClientProps {
-    listings: SafeListing[];
-    currentUser?: SafeUser | null;
-
+	listings: SafeListing[];
+	currentUser?: SafeUser | null;
 }
 
-const FavoritesClient = ({
-    listings,
-    currentUser
-}:FavoritesClientProps) => {
-    return (
-        <Container>
-            <Heading
-                title="Favorites"
-                subtitle="Your favorite listings"
-            
-            />
-        </Container>
-    );
-}
- 
+const FavoritesClient = ({ listings, currentUser }: FavoritesClientProps) => {
+	return (
+		<Container>
+			<Heading title="Favorites" subtitle="Your favorite listings" />
+			<div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+				{listings.map((listing) => (
+					<ListingCard
+						key={listing.id}
+						data={listing}
+						currentUser={currentUser}
+					/>
+				))}
+			</div>
+		</Container>
+	);
+};
+
 export default FavoritesClient;
