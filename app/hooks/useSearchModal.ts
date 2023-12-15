d4c349fae30e9344d8df3531ed61c0a16dba5768
abudusamad <abudusamed@gmail.com>
2023-12-15 00:50:33 +0000

    working on the search modal store to control the state of the searh items

---
+++ app/hooks/useSearchModal.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type SearchModalStore = {
+	isOpen: boolean;
+	onOpen: () => void;
+	onClose: () => void;
+};
+
+const useSearchModal = create<SearchModalStore>((set) => ({
+	isOpen: false,
+	onOpen: () => set({ isOpen: true }),
+	onClose: () => set({ isOpen: false }),
+}));
