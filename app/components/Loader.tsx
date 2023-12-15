1e731e4eaa83f266c76387c9067a96bc4d663ace
abudusamad <abudusamed@gmail.com>
2023-12-15 01:04:08 +0000

    Making the loading state for different pages

---
+++ app/components/Loader.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { MoonLoader } from "react-spinners";
+
+const Loader = () => {
+	return (
+		<div className="h-[70vh] flex flex-col justify-center items-center">
+			<MoonLoader size={150} color={"#36d7b7"} loading={true} />
+		</div>
+	);
+};
+
+export default Loader;
